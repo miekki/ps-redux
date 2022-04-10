1a883732086e6118fc83746e0fddbe236148f4e3
Maciej <maciejws@gmail.com>
2022-04-10 16:22:49 +0100

    Handling Async calls

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,14 +1,24 @@
 import * as types from "./actionsTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course: course };
-}
-
 export function loadCoursesSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses: courses,
+  };
+}
+
+export function createCourseSuccess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    course: course,
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    course: course,
   };
 }
 
@@ -18,6 +28,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
