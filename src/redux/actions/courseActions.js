86f12c49f2699c4de6673313e3f5cfc6b85971a5
Maciej <maciejws@gmail.com>
2022-04-21 16:18:23 +0100

    Add test and build proecess

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -46,7 +46,7 @@ export function loadCourses() {
 }
 
 export function saveCourse(course) {
-  return function (dispatch, getState) {
+  return function (dispatch) {
     dispatch(beginApiCall());
     return courseApi
       .saveCourse(course)
